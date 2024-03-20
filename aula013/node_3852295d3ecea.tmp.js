a8c39b9e1b5b6368611e62c31ad53e6fc1d83af2
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-03-20 17:19:20 -0300

    att aula 13

---
+++ aula013/node_3852295d3ecea.tmp.js
@@ -0,0 +1,23 @@
+let c = 1;
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+
+console.log('----------------------------------------------------------------')
+
+let contador = 1;
+
+do {
+    console.log(`Passo ${contador}`)
+} while (contador <= 6);
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
