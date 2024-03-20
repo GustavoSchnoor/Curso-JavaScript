bcdd649214f34662a2d427fabc65f510c189890c
GustavoSchnoor <153643888+GustavoSchnoor@users.noreply.github.com>
2024-03-20 18:15:57 -0300

    Att aula 014

---
+++ aula014/ambiente.js
@@ -0,0 +1,11 @@
+/*
+for ( Inicialização; Condição; Atualização) {
+    Bloco de comandos a serem realizados
+}        
+*/
+
+console.log('Vai começar...');
+for (let c = 1; c <= 4; c++) {
+    console.log(c);
+}
+console.log('Fim!');
